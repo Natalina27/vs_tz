4e88ce46118e832a0df081c37bdbbb31b6f0a669
Natalya Myunster <natalyamyunster@yahoo.com>
2020-11-18 15:22:21 +0100

    class component infinite scroll

--- src/index.js
+++ src/index.js
@@ -1,37 +1,60 @@
-import './App.css';
-// import {useEffect} from 'react';
-// import axios from 'axios';
-function App() {
-  // useEffect(() => {
-  //     axios({
-  //         url: 'https://pastebin.pl/view/raw/e1658aa0',
-  //         method: 'GET'
-  //             })
-  //                 .then((response) => {
-  //                     console.log('Data', response.data);
-  //                 })
-  //                 .catch('Oopps, there is an error')
-  // }, [])
-  const axios = require('axios');
-  axios.get('https://pastebin.pl/view/raw/e1658aa0')
-      .then(data => console.log(data))
-  // let componentDidMount;
-  // componentDidMount = () => {
-  //     axios({
-  //         url: 'https://pastebin.pl/view/raw/e1658aa0',
-  //         method: 'GET'
-  //     })
-  //         .then((response) => {
-  //             console.log('Data', response.data);
-  //         })
-  //         .catch('Oopps, there is an error')
-  // }
-  return (
-      <div className="App">
+import React from "react";
+import { render } from "react-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const style = {
+  height: 30,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8
+};
+
+class App extends React.Component {
+  state = {
+    items: Array.from({ length: 20 }),
+    hasMore: true
+  };
+
+  fetchMoreData = () => {
+    if (this.state.items.length >= 500) {
+      this.setState({ hasMore: false });
+      return;
+    }
+    // a fake async api call like which sends
+    // 20 more records in .5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 20 }))
+      });
+    }, 500);
+  };
+
+  render() {
+    return (
         <div>
-          <h2>Welcome to my App</h2>
+          <h1>demo: react-infinite-scroll-component</h1>
+          <hr />
+          <InfiniteScroll
+              dataLength={this.state.items.length}
+              next={this.fetchMoreData}
+              hasMore={this.state.hasMore}
+              loader={<h4>Loading...</h4>}
+              endMessage={
+                <p style={{ textAlign: "center" }}>
+                  <b>Yay! You have seen it all</b>
+                </p>
+              }
+          >
+            >
+            {this.state.items.map((i, index) => (
+                <div style={style} key={index}>
+                  div - #{index}
+                </div>
+            ))}
+          </InfiniteScroll>
         </div>
-      </div>
-  );
+    );
+  }
 }
-export default App;
+
+render(<App />, document.getElementById("root"));
